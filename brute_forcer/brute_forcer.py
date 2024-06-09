8b4121923cad29fc9ca6d26794a457fd35541437
codedsprit <roshan0x01@gmail.com>
2024-06-09 10:03:04 +0545

    chore(main): added some feature and look nice, though

--- brute_forcer/brute_forcer.py
+++ brute_forcer/brute_forcer.py
@@ -1,17 +1,42 @@
+#
+# Author: Bibash Japrel (github.com/bibashjaprel)
+# https://github.com/t3l3machus
+#
+
+# A simple yet fast tool for finding web directories.
+
 import requests
-import re,sys,argparse
+import re, sys, argparse
 import concurrent.futures
 from urllib.parse import urljoin
-#Check and parse the Argument
+
+
+''' Colors '''
+BLUE = '\033[0;38;5;12m'
+RED = '\033[1;31m'
+END = '\033[0m'
+BOLD = '\033[1m'
+
+
+# -------------- Arguments & Usage -------------- #
 parser = argparse.ArgumentParser()
+
 parser.add_argument("-u", "--url", help="Use -u or --url followed by a url")
 parser.add_argument("-w", "--wordlist", help="Use -w or --wordlist followed by wordlist")
-args = parser.parse_args()
+parser.add_argument("-i", "--port", help="shows 404 not found(default doesnt shows.)")
+args = parser.parse_args()  # arguments to be parsed
+
 if not args.url:
-    print(f"\033[0;31mPlease Provide a url use -h or --help to get the help message\033[0m")
+    print('''%s┓          ┏         
+┣┓┏┓┓┏╋┏┓━━╋┏┓┏┓┏┏┓┏┓
+┗┛┛ ┗┻┗┗   ┛┗┛┛ ┗┗ ┛
+   by @bibashjaprel <3 %s''' % (RED, END))
+    print()
+    print('''%s * 💻 web app directory brute forcer.%s''' % (BLUE, END))
     sys.exit(1)
 else:
-        url = args.url
+    url = args.url
+
 # Function to Check the Url is Valid or Invalid 
 def url_checker(url):
     url_regex = r'^https?:\/\/'
@@ -19,6 +44,7 @@ def url_checker(url):
         print("Invalid URL. Please provide a URL with http or https scheme.")
         sys.exit(1)  
     return url
+
 # Function to check if a directory exists on the target website
 def check_directory(url, directory):
     try:
@@ -39,7 +65,7 @@ def brute_force_directories(url, wordlist, num_threads=10):
         futures = [executor.submit(check_directory, url, directory) for directory in directories]
         try:
             for future in concurrent.futures.as_completed(futures):
-               future.result()
+                future.result()
         except KeyboardInterrupt:
             print("\nKeyboardInterrupt received. Terminating...")
             for future in futures:
@@ -47,8 +73,7 @@ def brute_force_directories(url, wordlist, num_threads=10):
             executor.shutdown(wait=False)
 
 if __name__ == "__main__":
-    url=args.url
+    url = args.url
     target_url = url_checker(url)
     wordlist_path = args.wordlist if args.wordlist else '../wordlist/common_directories.txt'
     brute_force_directories(target_url, wordlist_path)
-
